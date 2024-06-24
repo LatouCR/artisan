99f40756f7084973983c9ee42d1bf5abdf788015
LatouCR <dlatouches811@ulacit.ed.cr>
2024-06-23 22:27:16 -0600

    stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default async function HomePage() {
   console.log(posts)
 
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-slate-50 text-black">
+    <main className="flex min-h-screen flex-col items-center justify-center bg-slate-200 text-black">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
         <h1>trabajo en proceso....</h1>
 
