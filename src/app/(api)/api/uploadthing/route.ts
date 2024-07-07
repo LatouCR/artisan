d4879dfef34cab00af2fc703cd69e13a9b01fc67
LatouCR <dlatouches811@ulacit.ed.cr>
2024-07-06 20:45:31 -0600

    Fix?

--- src/app/(api)/api/uploadthing/route.ts
+++ src/app/(api)/api/uploadthing/route.ts
@@ -1,7 +1,11 @@
-import { createRouteHandler } from "uploadthing/server";
-
+/*import { createRouteHandler } from "uploadthing/next";
+ 
 import { ourFileRouter } from "./core";
-
+ 
+// Export routes for Next App Router
 export const { GET, POST } = createRouteHandler({
-    router: ourFileRouter,
-})
+  router: ourFileRouter,
+ 
+  // Apply an (optional) custom config:
+  // config: { ... },
+});*/
