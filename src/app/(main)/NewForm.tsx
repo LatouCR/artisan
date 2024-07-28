d36889480405d5c23ea16b52228babd6ceba03f7
LatouCR <dlatouches811@ulacit.ed.cr>
2024-07-28 14:31:46 -0600

    Tiptap Rich Editor

---
+++ src/app/(main)/NewForm.tsx
@@ -0,0 +1,49 @@
+"use client";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import Tiptap from "@/components/Tiptap";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+export const NewForm = () =>  {
+
+    const formSchema = z.object({
+        text: z.string().min(1, { message: "A Message is required" }),
+    })
+
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),  
+        mode: "onChange",
+        defaultValues: {
+            text: "",
+        }
+    })
+
+    function onSubmit(values: z.infer<typeof formSchema>) {
+        console.log("A new message was submitted with content: ",  values)
+    }
+
+    return (
+        <div className="w-full">
+            <Form {...form}>
+                <form onSubmit={form.handleSubmit(onSubmit)}>
+                    <FormField
+                        control={form.control}
+                        name="text"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel htmlFor={field.value}>New Post</FormLabel>
+                                    <FormControl>
+                                        <Tiptap description={field.value} onChange={field.onChange}></Tiptap>
+                                    </FormControl>
+                                    <FormMessage>{form.formState.errors.text?.message}</FormMessage>
+                                </FormItem>
+                        )}
+                    >
+                    </FormField>
+                </form>
+            </Form>
+        </div>
+    )
+
+}
--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,9 +1,12 @@
+import { NewForm } from "./NewForm";
+
 export default async function HomePage() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-center bg-slate-200 text-black">
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16 ">
         <h1>trabajo en proceso....</h1>
+        <NewForm/>
       </div>
     </main>
   );
